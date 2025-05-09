3d755c921635a7f3721452bab75dffac2ad9cfce
kamranvat <kamran.vat@gmail.com>
2025-05-09 10:42:57 +0200

    fix: pass (more) correct values into derivs

--- lab3example.py
+++ lab3example.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 
 def euler_integrate(
-    derivs,
-    x0,
-    t,
+    derivs, # function
+    x0, # initial state
+    t, # time
 ):
     x = np.empty((len(t), len(x0)))
 
@@ -85,7 +85,7 @@ def gating_fct_l(g_l, v, E_l):
     return i_l
 
 
-def HH_derivative(tau, last_state, I):
+def HH_derivative(tau, x0, I):
     # I is I_c (the step current)
     # c_m dv/dt = i_c - i_na - i_k - i_l
     # all the parameters:
@@ -96,35 +96,35 @@ def HH_derivative(tau, last_state, I):
     E_na = 50.0
     E_k = -90.0
     E_l = -65.0
-    v = last_state[0]
+
+    # get infs from current voltage
+    m_inf = lambda v: calc_x_inf(alpha_m(v), beta_m(v))
+    n_inf = lambda v: calc_x_inf(alpha_n(v), beta_n(v))
+    h_inf = lambda v: calc_x_inf(alpha_h(v), beta_h(v))
+
+    # tau with current voltage
+    tau_m = lambda v: calc_tau(alpha_m(v), beta_m(v))
+    tau_n = lambda v: calc_tau(alpha_n(v), beta_n(v))
+    tau_h = lambda v: calc_tau(alpha_h(v), beta_h(v))
 
     def derivs(t, x):
         # unpack x into V, m, n, h:
-        V, m, n, h = x
-        # tau
-        tau_m = lambda v: calc_tau(alpha_m(v), beta_m(v))
-        tau_n = lambda v: calc_tau(alpha_n(v), beta_n(v))
-        tau_h = lambda v: calc_tau(alpha_h(v), beta_h(v))
-        # get all infs from current voltage and return them
-        m_inf = lambda v: calc_x_inf(alpha_m(v), beta_m(v))
-        n_inf = lambda v: calc_x_inf(alpha_n(v), beta_n(v))
-        h_inf = lambda v: calc_x_inf(alpha_h(v), beta_h(v))
-        # do all gate updates ( the calc_xdtod function)
-        m = calc_xdot(m_inf(v), m_inf(v), tau_m(v))
-        n = calc_xdot(n_inf(v), n_inf(v), tau_n(v))
-        h = calc_xdot(h_inf(v), h_inf(v), tau_h(v))
-        # calculate all currents( gating_fct_na, gating_fct_k, gating_fct_l)
+        v, m, n, h = x
+
+        # do all gate updates ( the calc_xdot function)
+        mdot = calc_xdot(m_inf(v), m_inf(v), tau_m(v))
+        ndot = calc_xdot(n_inf(v), n_inf(v), tau_n(v))
+        hdot = calc_xdot(h_inf(v), h_inf(v), tau_h(v))
+
+        # calculate all currents (gating_fct_na, gating_fct_k, gating_fct_l)
         i_na = gating_fct_na(g_na, m, h, v, E_na)
         i_k = gating_fct_k(g_k, n, v, E_k)
         i_l = gating_fct_l(g_l, v, E_l)
         i_c = I(t)
+
         # then calculate dv_dt: I(t) - i_na - i_k - i_l / C_m
         vdot = (i_c - i_na - i_k - i_l) / C_m
-        # then calculate dm_dt, dn_dt, dh_dt:
-        mdot = (m_inf(v) - m) / tau_m(v)
-        ndot = (n_inf(v) - n) / tau_n(v)
-        hdot = (h_inf(v) - h) / tau_h(v)
-        # return array
+
         return np.array([vdot, mdot, ndot, hdot])
 
     return derivs
